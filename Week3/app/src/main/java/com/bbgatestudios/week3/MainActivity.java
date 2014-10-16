2d406fa6ef9261f9a1fd5cb8f01167b089a68d58
John Brandenburg <jmbburg26@fullsail.edu>
2014-10-16 15:56:25 -0400

    Added the "Add Contact" option in the settings menu

--- Week3/app/src/main/java/com/bbgatestudios/week3/MainActivity.java
+++ Week3/app/src/main/java/com/bbgatestudios/week3/MainActivity.java
@@ -52,9 +52,6 @@ public class MainActivity extends Activity implements MainFragment.ContactListen
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
